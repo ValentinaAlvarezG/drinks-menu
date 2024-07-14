1d40052a1ba1d43870891ff74557f467e1e0c7aa
alanezz <adrian.soto.suarez@gmail.com>
2024-07-14 01:00:11 -0400

    :hot-dog: add cocktails

---
+++ static/custom/saffitext.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const phrases = [
+        "El trabajo dignifica. 🛠️",
+        "Es difícil ser hombre. 👨🏻",
+        "Y si matamos unos zombies? 🧟"
+    ];
+
+    const textElement = document.getElementById('saffitext');
+
+    textElement.addEventListener('click', () => {
+        const randomIndex = Math.floor(Math.random() * phrases.length);
+        textElement.innerText = phrases[randomIndex];
+    });
+});
